4ce6cef103f1d880cd9031290c9a2cf4b357a300
marka <marka@LAPTOP-INOCT3S4>
2020-04-21 16:18:30 -0300

    Criação do EndPoint getAllPostagens

--- blogPessoal/src/app/service/postagem.service.ts
+++ blogPessoal/src/app/service/postagem.service.ts
@@ -1,9 +1,19 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PostagemService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  /*CRUD
+  é um acronomo de palavras CREAT, READY, UPDATE e DELETE
+  */
+
+  getAllPostagem() {
+    return this.http.get('http://31.220.57.14:8080/postagens')
+  }
+
 }
